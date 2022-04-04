038166c33c67b2035da7a5fbe44b986484672920
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-04 19:51:14 -0400

    revert back to old code.

--- main.py
+++ main.py
@@ -1,61 +1,28 @@
 from time import sleep
 from control.robot import robot
-import RPi.GPIO as GPIO
-import asyncio
 from sshkeyboard import listen_keyboard
-import random
+import RPi.GPIO as GPIO
+import threading
 
 robot = robot()
 
-distance = 500
+
+def press(key):
+    if key == "up":
+        robot.forward()
+    elif key == "down":
+        robot.stop()
+    elif key == "left":
+        robot.left.turn(distance=500)
+    elif key == "right":
+        robot.right.turn(distance=500)
 
 
-async def keyPress(key):
-    if key == 'w':
-        robot.leftInput = 1
-        robot.rightInput = 1
-        print("w")
-    elif key == 'a':
-        robot.rightInput = 1
-        robot.leftInput = -1
-        print("a")
-    elif key == 'd':
-        robot.leftInput = 1
-        robot.rightInput = -1
-        print("d")
-    elif key == 's':
-        robot.leftInput = -1
-        robot.rightInput = -1
-        print("s")
-    else:
-        robot.leftInput = 0
-        robot.rightInput = 0
-        print("no input")
-
-    await asyncio.sleep(0.001)
-
-
-async def command():
-    robot.leftInput = random.randint(-1, 1)
-    robot.rightInput = random.randint(-1, 1)
-
-    robot.left.turn(distance=robot.leftInput * distance)
-    robot.right.turn(distance=robot.rightInput * distance)
-    await asyncio.sleep(0.001)
-    print("command loop")
-
-
-async def main():
-    while True:
-        await command()
-
-# Rest of code should be good, just need to figure out how to get listen_keyboard to run async
 try:
     print("Use up,left,right and down. Down means stop motors")
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    listen_keyboard(on_press=press)
 
+# Once finished clean everything up
 except KeyboardInterrupt:
     robot.stop()
     print("cleanup")
