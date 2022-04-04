7611d4ba2c3a3f3765af25ae9d5f50377e7869bf
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-04 14:36:05 -0400

    test

--- test.py
+++ test.py
@@ -3,37 +3,19 @@ from control.robot import robot
 from sshkeyboard import listen_keyboard
 import RPi.GPIO as GPIO
 import threading
-import asyncio
 
 robot = robot()
-
-left = 0
-right = 0
-distance = 500
 
 
 def press(key):
     if key == "up":
-        left = 1
-        right = 1
-
-    if key == "left":
-        right = 1
-        left = -1
-
-    if key == "right":
-        left = 1
-        right = -1
-
-    if key == "back":
-        left = -1
-        right = -1
-
-
-def command():
-    while True:
-        robot.left.turn(left * distance)
-        robot.right.turn(right * distance)
+        robot.forward()
+    elif key == "down":
+        robot.stop()
+    elif key == "left":
+        robot.left.turn(distance=500)
+    elif key == "right":
+        robot.right.turn(distance=500)
 
 
 try:
