8203104ad8727696c68cf1a24d2740f050bc4a21
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-04 14:58:17 -0400

    test

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ distance = 500
 
 
 async def keyPress():
-    if keyboard.is_pressed('w'):
+    if keyboard.read_key() == "w":
         robot.leftInput = 1
         robot.rightInput = 1
         print("w")
-    elif keyboard.is_pressed('a'):
+    elif keyboard.read_key() == "a":
         robot.rightInput = 1
         robot.leftInput = -1
         print("a")
-    elif keyboard.is_pressed('d'):
+    elif keyboard.read_key() == "d":
         robot.leftInput = 1
         robot.rightInput = -1
         print("d")
-    elif keyboard.is_pressed('s'):
+    elif keyboard.read_key() == "s":
         robot.leftInput = -1
         robot.rightInput = -1
         print("s")
     else:
         robot.leftInput = 0
         robot.rightInput = 0
-        print("no input")
+        print("no input " + keyboard.read_key())
 
     #print("key press; inputs: " + str(robot.leftInput) + "; " + str(robot.rightInput))
 
