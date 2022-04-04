6c85a82bf909ce97d1b08bf6ec50931a398a7a3f
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-04 15:04:20 -0400

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from time import sleep
 from control.robot import robot
 import RPi.GPIO as GPIO
 import asyncio
-import keyboard
+from sshkeyboard import listen_keyboard
 
 robot = robot()
 
@@ -10,26 +10,28 @@ distance = 500
 
 
 async def keyPress():
-    if keyboard.read_key() == "w":
+    if keyboard.is_pressed('w'):
         robot.leftInput = 1
         robot.rightInput = 1
         print("w")
-    elif keyboard.read_key() == "a":
+    elif keyboard.is_pressed('a'):
         robot.rightInput = 1
         robot.leftInput = -1
         print("a")
-    elif keyboard.read_key() == "d":
+    elif keyboard.is_pressed('d'):
         robot.leftInput = 1
         robot.rightInput = -1
         print("d")
-    elif keyboard.read_key() == "s":
+    elif keyboard.is_pressed('s'):
         robot.leftInput = -1
         robot.rightInput = -1
         print("s")
     else:
         robot.leftInput = 0
         robot.rightInput = 0
-        print("no input " + keyboard.read_key())
+        print("no input")
+
+    await asyncio.sleep(0.001)
 
     #print("key press; inputs: " + str(robot.leftInput) + "; " + str(robot.rightInput))
 
@@ -37,13 +39,14 @@ async def keyPress():
 async def command():
     await robot.left.turn(1, robot.leftInput * distance)
     await robot.right.turn(1, robot.rightInput * distance)
+    await asyncio.sleep(0.001)
     #print("command loop")
 
 
 async def main():
+    listen_keyboard(on_press=press)
     while True:
         await command()
-        await keyPress()
 
 try:
     print("Use up,left,right and down. Down means stop motors")
