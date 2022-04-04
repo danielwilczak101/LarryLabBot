678a7dea10682377b7cb83d92cb5ed2d297e1de4
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-04 13:14:45 -0400

    update turn radius

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def press(key):
     elif key == "down":
         robot.stop()
     elif key == "left":
-        robot.left.turn(1000)
+        robot.left.turn(5000)
     elif key == "right":
-        robot.right.turn(1000)
+        robot.right.turn(5000)
 
 
 try:
