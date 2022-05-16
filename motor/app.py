578b959a9032a1d8fca02ee9462783ce06d2dd56
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-05-16 14:21:59 -0500

    Added motor code and example flask code.

---
+++ motor/app.py
@@ -0,0 +1,28 @@
+from adafruit_motorkit import MotorKit
+from flask import Flask
+
+kit = MotorKit()
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Use routes: /start, /stop, /reverse.'
+
+
+@app.route('/start')
+def hello_world():
+    kit.motor1.throttle = 0.5
+    return 'Motor 1 should start'
+
+
+@app.route('/stop')
+def hello_world():
+    kit.motor1.throttle = 0
+    return 'Motor 1 should stop'
+
+
+@app.route('/reverse')
+def hello_world():
+    kit.motor1.throttle = -0.5
+    return 'Motor 1 should reverse'
