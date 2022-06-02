e2cf5da1b063e48bcdd283cfa99bd8df4ee59e2c
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-06-01 23:25:57 -0500

    updated

--- cart.py
+++ cart.py
@@ -1,20 +1,21 @@
-import RPi.GPIO as GPIO
-from time import sleep
 
+from flask import Flask, render_template
+from time import sleep
+import RPi.GPIO as GPIO
+import socket
+
+app = Flask(__name__)
 
 stepper_pwm_pin = 19
 enable = 20
 dir = 21
-
 motor_pin = 12
-
 
 GPIO.setmode(GPIO.BCM)  # set pin numbering system
 
 GPIO.setup(stepper_pwm_pin, GPIO.OUT)
 GPIO.setup(enable, GPIO.OUT)
 GPIO.setup(dir, GPIO.OUT)
-
 GPIO.setup(motor_pin, GPIO.OUT)
 
 GPIO.output(enable, GPIO.HIGH)
@@ -24,30 +25,40 @@ GPIO.output(dir, GPIO.HIGH)
 stepper_pwm = GPIO.PWM(stepper_pwm_pin, 5000)
 stepper_pwm.start(0)  # start PWM of required Duty Cycle
 
-motor_pwm = GPIO.PWM(motor_pin, 5000)  # create PWM instance with frequency
+motor_pwm = GPIO.PWM(motor_pin, 10_000)  # create PWM instance with frequency
 motor_pwm.start(0)  # start PWM of required Duty Cycle
 
 
-print("Running")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
-def direction(dir):
-    """
-    This function sets the direction of the stepper motor.
-    Right = 1 
-    Left = 0
-    """
-    if dir == 1:
-        GPIO.output(dir, GPIO.HIGH)
-    elif dir == 0:
-        GPIO.output(dir, GPIO.LOW)
-    else:
-        print("Direction Error")
+@app.route('/forward')
+def forward():
+    motor_pwm.ChangeDutyCycle(100)
+    print("forward")
+    return 'F'
 
 
-while True:
+@app.route('/left')
+def left():
+    GPIO.output(dir, GPIO.HIGH)
+    stepper_pwm.ChangeDutyCycle(100)
+    print("left")
+    return 'L'
 
-    for duty in range(0, 101, 1):
-        # provide duty cycle in the range 0-100
-        motor_pwm.ChangeDutyCycle(duty)
-        sleep(0.001)
+
+@app.route('/right')
+def right():
+    GPIO.output(dir, GPIO.LOW)
+    stepper_pwm.ChangeDutyCycle(100)
+    print("right")
+    return 'R'
+
+
+@app.route('/stop')
+def stop():
+    motor_pwm.ChangeDutyCycle(100)
+    print("stop")
+    return 'S'
