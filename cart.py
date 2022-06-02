b79a9a1f4302dc41e4d761b3acbcb3746b75ab76
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-06-01 21:52:03 -0500

    yeeet

--- cart.py
+++ cart.py
@@ -15,13 +15,14 @@ GPIO.setup(dir, GPIO.OUT)
 GPIO.output(enable, GPIO.HIGH)
 GPIO.output(dir, GPIO.HIGH)
 
-pi_pwm = GPIO.PWM(pwm, 1000)  # create PWM instance with frequency
+pi_pwm = GPIO.PWM(pwm, 5000)  # create PWM instance with frequency
 pi_pwm.start(0)  # start PWM of required Duty Cycle
 
 
 print("Running")
 
 while True:
+
     for duty in range(0, 101, 1):
         pi_pwm.ChangeDutyCycle(duty)  # provide duty cycle in the range 0-100
-        sleep(0.0001)
+        sleep(0.001)
