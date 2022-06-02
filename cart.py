ae6da2428b944e3c2a2b669d40b5feb9ab412467
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-06-01 21:34:51 -0500

    Added state for enable and dir

--- cart.py
+++ cart.py
@@ -2,11 +2,20 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 ledpin = 19				# PWM pin connected to LED
-GPIO.setwarnings(False)  # disable warnings
+
 GPIO.setmode(GPIO.BOARD)  # set pin numbering system
 GPIO.setup(ledpin, GPIO.OUT)
+
+# Enable Pin
+GPIO.setup(20, GPIO.OUT, initial=1)
+# Direction pin
+GPIO.setup(21, GPIO.OUT, initial=1)
+
 pi_pwm = GPIO.PWM(ledpin, 1000)  # create PWM instance with frequency
 pi_pwm.start(0)  # start PWM of required Duty Cycle
+
+print("Running")
+
 while True:
     for duty in range(0, 101, 1):
         pi_pwm.ChangeDutyCycle(duty)  # provide duty cycle in the range 0-100
