b13f6389219af571d0a13cd9b9e3fdeaf756577b
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-06-01 22:42:15 -0500

    added motor

--- cart.py
+++ cart.py
@@ -2,27 +2,52 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-pwm = 19
+stepper_pwm_pin = 19
 enable = 20
 dir = 21
 
+motor_pin = 12
+
+
 GPIO.setmode(GPIO.BCM)  # set pin numbering system
 
-GPIO.setup(pwm, GPIO.OUT)
+GPIO.setup(stepper_pwm_pin, GPIO.OUT)
 GPIO.setup(enable, GPIO.OUT)
 GPIO.setup(dir, GPIO.OUT)
+
+GPIO.setup(motor_pin, GPIO.OUT)
 
 GPIO.output(enable, GPIO.HIGH)
 GPIO.output(dir, GPIO.HIGH)
 
-pi_pwm = GPIO.PWM(pwm, 5000)  # create PWM instance with frequency
-pi_pwm.start(0)  # start PWM of required Duty Cycle
+# create PWM instance with frequency
+stepper_pwm = GPIO.PWM(stepper_pwm_pin, 5000)
+stepper_pwm.start(0)  # start PWM of required Duty Cycle
+
+motor_pwm = GPIO.PWM(motor_pin, 5000)  # create PWM instance with frequency
+motor_pwm.start(0)  # start PWM of required Duty Cycle
 
 
 print("Running")
 
+
+def direction(dir):
+    """
+    This function sets the direction of the stepper motor.
+    Right = 1 
+    Left = 0
+    """
+    if dir == 1:
+        GPIO.output(dir, GPIO.HIGH)
+    elif dir == 0:
+        GPIO.output(dir, GPIO.LOW)
+    else:
+        print("Direction Error")
+
+
 while True:
 
     for duty in range(0, 101, 1):
-        pi_pwm.ChangeDutyCycle(duty)  # provide duty cycle in the range 0-100
+        # provide duty cycle in the range 0-100
+        motor_pwm.ChangeDutyCycle(duty)
         sleep(0.001)
