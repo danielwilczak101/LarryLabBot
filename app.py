584f92c0592b9b389e13d5a5c208fad0722a1ee8
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-07-26 21:16:53 -0400

    updated controls

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import socket
 kit = MotorKit()
 app = Flask(__name__)
 
-throttle = 1
+throttle = 0.7
 
 
 @app.route('/')
@@ -16,11 +16,8 @@ def index():
 
 @app.route('/forward')
 def forward():
-    kit.motor2.throttle = 0
-    kit.motor4.throttle = 0
-
     kit.motor1.throttle = throttle
-    kit.motor3.throttle = -throttle
+    kit.motor2.throttle = throttle
 
     print("forward")
     return 'F'
@@ -28,33 +25,24 @@ def forward():
 
 @app.route('/reverse')
 def reverse():
-    kit.motor2.throttle = 0
-    kit.motor4.throttle = 0
-
     kit.motor1.throttle = -throttle
-    kit.motor3.throttle = throttle
+    kit.motor2.throttle = -throttle
     print("reverse")
     return 'R'
 
 
 @app.route('/left')
 def left():
-    kit.motor1.throttle = 0
-    kit.motor3.throttle = 0
-
+    kit.motor1.throttle = -throttle
     kit.motor2.throttle = throttle
-    kit.motor4.throttle = -throttle
     print("left")
     return 'L'
 
 
 @app.route('/right')
 def right():
-    kit.motor1.throttle = 0
-    kit.motor3.throttle = 0
-
+    kit.motor1.throttle = throttle
     kit.motor2.throttle = -throttle
-    kit.motor4.throttle = throttle
     print("right")
     return 'R'
 
@@ -63,29 +51,6 @@ def right():
 def stop():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
-    kit.motor3.throttle = 0
-    kit.motor4.throttle = 0
+
     print("stop")
     return 'S'
-
-
-@app.route('/rotate/right')
-def rotate_right():
-    kit.motor1.throttle = 0
-    kit.motor3.throttle = throttle
-
-    kit.motor2.throttle = 0
-    kit.motor4.throttle = throttle
-    print("rotate right")
-    return 'RR'
-
-
-@app.route('/rotate/left')
-def rotate_left():
-    kit.motor1.throttle = -throttle
-    kit.motor3.throttle = 0
-
-    kit.motor2.throttle = -throttle
-    kit.motor4.throttle = 0
-    print("rotate left")
-    return 'RL'
